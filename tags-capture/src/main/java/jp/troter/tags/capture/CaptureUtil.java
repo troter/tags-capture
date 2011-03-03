8e8ebe2e3652fb8fe119099a21994ff0086a59f1
troter <trot.thunder@gmail.com>
2011-03-03 10:32:29 +0900

    add license. version 1.0.0

--- tags-capture/src/main/java/jp/troter/tags/capture/CaptureUtil.java
+++ tags-capture/src/main/java/jp/troter/tags/capture/CaptureUtil.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright 2011 Takumi IINO.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package jp.troter.tags.capture;
 
 import java.util.LinkedList;
+
+import org.apache.commons.lang.StringUtils;
 
 /**
  * キャプチャで利用するユーティリティ
@@ -16,18 +33,7 @@ public class CaptureUtil {
      * @return
      */
     public static String captureName(String name) {
-        return CaptureUtil.CONTENTS_FOR_PREFIX + capitalize(name);
-    }
-
-    public static String capitalize(String str) {
-        int strLen;
-        if (str == null || (strLen = str.length()) == 0) {
-            return str;
-        }
-        return new StringBuffer(strLen)
-            .append(Character.toTitleCase(str.charAt(0)))
-            .append(str.substring(1))
-            .toString();
+        return CaptureUtil.CONTENTS_FOR_PREFIX + StringUtils.capitalize(name);
     }
 
     public static String nullToEmpty(String string) {
--- tags-capture/src/main/java/jp/troter/tags/capture/ContentForTag.java
+++ tags-capture/src/main/java/jp/troter/tags/capture/ContentForTag.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 Takumi IINO.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package jp.troter.tags.capture;
 
 import java.util.LinkedList;
@@ -15,9 +30,6 @@ import org.apache.commons.lang.StringUtils;
  * <code>body</code>に含めたJSP、もしくは <code>value</code> 属性の値をキャプチャします。<br />
  * contentForタグでキャプチャした文字列はyieldタグで出力できます。
  * </p>
- * 
- * @author troter
- * @author yoppi
  */
 public class ContentForTag extends BodyTagSupport {
 
